20f1e5e80f0894e220b1d752343d075c776b50a5
Heni <henij@8276gmail.com>
2024-04-09 06:51:26 +0530

    8/4

--- lib/screen/clockScreen/clock.dart
+++ lib/screen/clockScreen/clock.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:clock_flutter/global.dart';
 import 'package:flutter/material.dart';
 
 class Clock extends StatefulWidget {
@@ -22,7 +21,8 @@ class _ClockState extends State<Clock> {
     super.initState();
     getTime();
   }
-  int i=1;
+
+  int i = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +45,9 @@ class _ClockState extends State<Clock> {
                       height: 200,
                       width: 200,
                       decoration: const BoxDecoration(
-                          gradient: LinearGradient(begin:Alignment.centerLeft,end:Alignment.centerRight ,
-                              colors:[
-                            Colors.black,
-                            Colors.white30,
-                            Colors.black12,
-                          ]), shape: BoxShape.circle),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(image: AssetImage("assets/image/dc.jpg"))
+                      ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -75,7 +72,7 @@ class _ClockState extends State<Clock> {
                       scale: 6,
                       child: CircularProgressIndicator(
                           value: time.second / 60,
-                          color: Colors.pink,
+                          color: Colors.amber,
                           strokeCap: StrokeCap.round,
                           backgroundColor: Colors.white12),
                     ),
@@ -84,31 +81,34 @@ class _ClockState extends State<Clock> {
               ),
               //Analog Clock
               Visibility(
-                visible: isanalog ,
+                visible: isanalog,
                 child: Stack(
-                  children: List.generate(60, (index) =>
-                     Transform.rotate(
-                      angle: ((pi * 2)*index)/60,
+                  children: List.generate(
+                    60,
+                    (index) => Transform.rotate(
+                      angle: ((pi * 2) * index) / 60,
                       child: Divider(
                         color: Colors.white,
                         thickness: 2,
-                        endIndent: index%5==0?MediaQuery.sizeOf(context).width*0.90:MediaQuery.sizeOf(context).width*0.95,
+                        endIndent: index % 5 == 0
+                            ? MediaQuery.sizeOf(context).width * 0.90
+                            : MediaQuery.sizeOf(context).width * 0.95,
                       ),
-                                         ),
+                    ),
                   ),
                 ),
               ),
               //Second
               Visibility(
-                visible: isanalog ,
+                visible: isanalog,
                 child: Transform.rotate(
-                  angle: ((pi*2)*time.second)/60,
+                  angle: ((pi * 2) * time.second) / 60,
                   child: Transform.rotate(
-                    angle: pi/2,
+                    angle: pi / 2,
                     child: Divider(
                       color: Colors.red,
                       thickness: 2,
-                      endIndent: MediaQuery.sizeOf(context).width*0.50,
+                      endIndent: MediaQuery.sizeOf(context).width * 0.50,
                     ),
                   ),
                 ),
@@ -117,14 +117,14 @@ class _ClockState extends State<Clock> {
               Visibility(
                 visible: isanalog,
                 child: Transform.rotate(
-                  angle: ((pi*2)*time.minute)/60,
+                  angle: ((pi * 2) * time.minute) / 60,
                   child: Transform.rotate(
-                    angle: pi/2,
+                    angle: pi / 2,
                     child: Divider(
                       color: Colors.amber,
                       thickness: 2,
                       indent: 50,
-                      endIndent: MediaQuery.sizeOf(context).width*0.50,
+                      endIndent: MediaQuery.sizeOf(context).width * 0.50,
                     ),
                   ),
                 ),
@@ -133,14 +133,14 @@ class _ClockState extends State<Clock> {
               Visibility(
                 visible: isanalog,
                 child: Transform.rotate(
-                 angle: ((pi*2)*time.hour)/12,
+                  angle: ((pi * 2) * time.hour) / 12,
                   child: Transform.rotate(
-                    angle: pi/2,
+                    angle: pi / 2,
                     child: Divider(
                       color: Colors.green,
                       thickness: 2,
                       indent: 80,
-                      endIndent: MediaQuery.sizeOf(context).width*0.50,
+                      endIndent: MediaQuery.sizeOf(context).width * 0.50,
                     ),
                   ),
                 ),
@@ -152,37 +152,78 @@ class _ClockState extends State<Clock> {
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
+                    Container(
+                      height: 250,
+                      width: 250,
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [
+                                Color(0xff2068B0),
+                                Color(0xff4A9BD4),
+                                Color(0xff3081C0),
+                              ]),
+                          shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black,
+                            spreadRadius: 5,
+                            blurRadius: 3
+                          ),
+                        ]
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "${time.hour}:${time.minute}:${time.second}",
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                          Text(
+                            "${time.day}/${time.month}/${time.year}",
+                            style: const TextStyle(
+                                fontSize: 25,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
                     //Second
                     Container(
-                      height: 200,
-                      width: 200,
-                        child: CircularProgressIndicator(
-                          value: time.second*1/60,
-                          color: Colors.pink,
-                          strokeCap: StrokeCap.round,
-                          strokeWidth: 16,
-                        ),
+                      height: 250,
+                      width: 250,
+                      child: CircularProgressIndicator(
+                        value: time.second * 1 / 60,
+                        color: const Color(0xffFE7E03),
+                        strokeCap: StrokeCap.round,
+                        strokeWidth: 19,
                       ),
+                    ),
                     //Minute
                     Container(
-                      height: 160,
-                      width: 160,
+                      height: 210,
+                      width: 210,
                       child: CircularProgressIndicator(
-                        value: time.minute/60,
-                        color: Colors.green,
+                        value: time.minute / 60,
+                        color: Colors.white,
                         strokeCap: StrokeCap.round,
-                        strokeWidth: 17,
+                        strokeWidth: 20,
                       ),
                     ),
                     //Hour
                     Container(
-                      height: 120,
-                      width: 120,
+                      height: 170,
+                      width: 170,
                       child: CircularProgressIndicator(
-                        value: time.hour/12,
-                        color: Colors.amber,
+                        value: time.hour / 12,
+                        color: const Color(0xff007C1E),
                         strokeCap: StrokeCap.round,
-                        strokeWidth: 18,
+                        strokeWidth: 20,
                       ),
                     ),
                   ],
@@ -197,89 +238,82 @@ class _ClockState extends State<Clock> {
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
                   Container(
                     alignment: Alignment.topCenter,
                     height: 100,
                     width: 100,
-                    decoration: BoxDecoration(
-                      color: Colors.black26,
-                      shape: BoxShape.circle
-                    ),
+                    decoration: const BoxDecoration(
+                        color: Colors.black26, shape: BoxShape.circle),
                   ),
-                  Text("Your Name"),
-                  SizedBox(height: 20,),
+                  const Text("Your Name"),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   //Digital watch
                   Row(
                     children: [
                       //Digital Watch
-                      Text("Digital Watch",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 17
-                        ),
+                      const Text(
+                        "Digital Watch",
+                        style: TextStyle(color: Colors.black, fontSize: 17),
                       ),
-                      Spacer(),
-                      ElevatedButton(onPressed: (){
-                        setState(() {
-                          isdigital=true;
-                          isanalog=false;
-                          isstrap=false;
-                        });
-                        Navigator.pop(context);
-                      },
-                          child: Icon(Icons.watch_later_outlined))
+                      const Spacer(),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isdigital = true;
+                              isanalog = false;
+                              isstrap = false;
+                            });
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(Icons.watch_later_outlined))
                     ],
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                  const SizedBox(height: 10),
                   //Analog Clock
                   Row(
                     children: [
                       //Digital Watch
-                      Text("Analog Clock",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 17
-                        ),
+                      const Text(
+                        "Analog Clock",
+                        style: TextStyle(color: Colors.black, fontSize: 17),
                       ),
-                      Spacer(),
-                      ElevatedButton(onPressed: (){
-                        setState(() {
-                          isdigital=false;
-                          isanalog=true;
-                          isstrap=false;
-                        });
-                        Navigator.pop(context);
-                      },
+                      const Spacer(),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isdigital = false;
+                              isanalog = true;
+                              isstrap = false;
+                            });
+                            Navigator.pop(context);
+                          },
                           child: Icon(Icons.watch_later_outlined))
                     ],
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
+                  const SizedBox(height: 10),
                   //Strap Watch
                   Row(
                     children: [
                       //Digital Watch
-                      Text("Strap Watch",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 17
-                        ),
+                      const Text(
+                        "Strap Watch",
+                        style: TextStyle(color: Colors.black, fontSize: 17),
                       ),
-                      Spacer(),
-                      ElevatedButton(onPressed: (){
-                        setState(() {
-                          isdigital=false;
-                          isanalog=false;
-                          isstrap=true;
-                        });
-                        Navigator.pop(context);
-                      },
+                      const Spacer(),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              isdigital = false;
+                              isanalog = false;
+                              isstrap = true;
+                            });
+                            Navigator.pop(context);
+                          },
                           child: Icon(Icons.watch_later_outlined))
                     ],
                   ),
